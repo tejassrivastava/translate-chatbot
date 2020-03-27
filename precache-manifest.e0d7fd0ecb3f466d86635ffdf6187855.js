7ef29dbde2b0baaa2b770cdc08050e92a0346bdb
tejas srivastava <tejassrivastava>
2020-03-27 20:43:27 +0530

    Updates

---
+++ precache-manifest.e0d7fd0ecb3f466d86635ffdf6187855.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c69d5369ee7251420a5eafbd063c4d",
+    "url": "/ck12/editor.worker.js"
+  },
+  {
+    "revision": "fb85bfb81933303ef143e83d9ebf5d7e",
+    "url": "/ck12/index.html"
+  },
+  {
+    "revision": "53651180a3bf14336a75",
+    "url": "/ck12/static/css/2.93692b17.chunk.css"
+  },
+  {
+    "revision": "7c15332d24446977f91c",
+    "url": "/ck12/static/css/main.88020ff7.chunk.css"
+  },
+  {
+    "revision": "53651180a3bf14336a75",
+    "url": "/ck12/static/js/2.d78a60f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ck12/static/js/2.d78a60f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153d102a32f86f8c028c",
+    "url": "/ck12/static/js/3.0a259f82.chunk.js"
+  },
+  {
+    "revision": "7c15332d24446977f91c",
+    "url": "/ck12/static/js/main.83c0769b.chunk.js"
+  },
+  {
+    "revision": "4ff019c85399d6025b2d",
+    "url": "/ck12/static/js/runtime-main.5190b579.js"
+  },
+  {
+    "revision": "a609dc0f334a7d4e64205247c4e8b97c",
+    "url": "/ck12/static/media/codicon.a609dc0f.ttf"
+  }
+]);
